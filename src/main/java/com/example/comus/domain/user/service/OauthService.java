aeddebfd212132f9c0e1b0d2b5c9539ae916c159
hysong4u <hayeon_song@naver.com>
2024-08-13 03:48:13 +0900

    [feat] 불필요한 class 삭제

--- src/main/java/com/example/comus/domain/user/service/OauthService.java
+++ src/main/java/com/example/comus/domain/user/service/OauthService.java
@@ -15,7 +15,6 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @RequiredArgsConstructor
