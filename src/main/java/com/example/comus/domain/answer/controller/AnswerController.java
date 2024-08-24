24bb25e2dd29877bf9b4ff1b100e0e86d301be88
hysong4u <hayeon_song@naver.com>
2024-08-24 22:21:33 +0900

    [fix] 날짜 response에 추가

--- src/main/java/com/example/comus/domain/answer/controller/AnswerController.java
+++ src/main/java/com/example/comus/domain/answer/controller/AnswerController.java
@@ -9,12 +9,17 @@ import com.example.comus.domain.block.entity.Block;
 import com.example.comus.domain.block.service.BlockService;
 import com.example.comus.domain.question.dto.response.QuestionListResponseDto;
 import com.example.comus.domain.question.service.QuestionService;
+import com.example.comus.domain.signlanguage.dto.response.SignLanguageInfoAndDateResponseDto;
+import com.example.comus.domain.signlanguage.dto.response.SignLanguageInfoResponseDto;
+import com.example.comus.domain.signlanguage.service.SignLanguageService;
 import com.example.comus.global.common.SuccessResponse;
 import com.example.comus.global.config.auth.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -24,13 +29,22 @@ public class AnswerController {
     private final AnswerService answerService;
     private final QuestionService questionService;
     private final BlockService blockService;
+    private final SignLanguageService signLanguageService;
+
 
     @PostMapping
     public ResponseEntity<?> createAnswer(@UserId Long userId, @RequestBody AnswerRequestDto answerRequest) {
         long anwserId = answerService.createAnswer(userId, answerRequest);
         blockService.save(anwserId);
 
-        return SuccessResponse.created(null);
+        String answer = answerRequest.answerContent();
+        List<SignLanguageInfoResponseDto> signLanguageInfo= signLanguageService.getSignLanguage(answer);
+        LocalDateTime answerDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy.MM.dd");
+        String formattedDate = answerDate.format(formatter);
+        SignLanguageInfoAndDateResponseDto signLanguageInfoAndDate = new SignLanguageInfoAndDateResponseDto(signLanguageInfo, formattedDate);
+
+        return SuccessResponse.ok(signLanguageInfoAndDate);
     }
 
     @GetMapping("/{questionId}")
