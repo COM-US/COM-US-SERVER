9cd23bbff07c24517a5086c8aa6e5a8a830cb1e6
hysong4u <hayeon_song@naver.com>
2025-01-30 02:19:28 +0900

    [add] #3 선택형 답변조회 response에 오늘 날짜 추가

--- src/main/java/com/example/comus/domain/question/dto/response/QuestionResponseDto.java
+++ src/main/java/com/example/comus/domain/question/dto/response/QuestionResponseDto.java
@@ -3,11 +3,13 @@ package com.example.comus.domain.question.dto.response;
 import com.example.comus.domain.question.entity.AnswerType;
 import com.example.comus.domain.question.entity.Category;
 
+import java.time.LocalDate;
+
 public record QuestionResponseDto(
         Long id,
+        LocalDate answerDate,
         Category category,
         AnswerType answerType,
         String questionContent
 ){
-
 }
